a1c329ed56b2238201e2280f677e5f2e7f7609e7
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2021-11-19 19:26:48 -0800

    project

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,3 +1,42 @@
+import {Vehicle}  from "./vehicleBaseClass";
+
+class Car extends Vehicle {
+    constructor (make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    }
+    start() {
+        if(this.fuel > 0){
+            console.log(this.make + this.model + ' enginge started.')
+        } else {
+            console.log(this.make + this. model + ' does not have enough fuel');
+        }
+    }
+    loadPassenger(num) {
+        if(this.passenger < this.maximumPassengers) {
+            if ((num + this.passenger) <= this.maximumPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + ' ' + this.make + ' does not have enough space to take all the passnegers.');
+            }
+        }else {
+            console.log(this.model + ' ' + this.make + ' is full.');
+        }
+    }
+    scheduleService(mileage) {
+        if(this.mileage > 30000) {
+           this.scheduleService == true
+           return this.scheduleService;
+        }
+    };
+}
+
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
 
